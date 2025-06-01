8abb9a5932dce42734209268f3feec111f63fc7f
chinwe <chinwe122@gmail.com>
2025-06-01 08:45:41 +0800

    feat(demo): 添加 jOOQ 示例
    - 在 pom.xml 中添加 H2 数据库和 Spring Boot jOOQ 启动器依赖
    - 在 TestController 中添加 jOOQ 测试接口
    - 配置 application.properties 文件以使用 H2 数据库
    - 更新 test.http 文件，添加 jOOQ 测试接口的请求示例

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.vo.DelayVo;
 
+import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    
+    @Resource
+    private DSLContext dsl;
     
     @GetMapping("/hello")
     public String sayHello() {
@@ -28,5 +34,12 @@ public class TestController {
     public String delay(@Valid @RequestBody DelayVo delayVo) throws InterruptedException {
         Thread.sleep(delayVo.getSecond() * 1000);
         return String.format("Delayed for %d seconds", delayVo.getSecond());
+    }
+
+    @PostMapping("/jooq")
+    public String jooq() {
+        return dsl.select(DSL.field("CURRENT_TIMESTAMP", String.class))
+            .from(DSL.table("DUAL"))
+            .fetchOne(0, String.class);
     }
 }
