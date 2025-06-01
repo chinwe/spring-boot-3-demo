2cbe71fb6de16c57093f807175f614ff63d7925d
chinwe <chinwe122@gmail.com>
2025-06-01 11:55:09 +0800

    build(pom): 更新项目依赖
    - 移除原始的 spring-boot-starter-test 依赖
    - 添加 spring-boot-starter-data-redis 依赖
    - 重新添加 spring-boot-starter-test 依赖
    - 添加 testcontainers 和 junit-jupiter 依赖用于测试

---
+++ src/test/java/com/example/demo/TestContainerExampleTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+public class TestContainerExampleTest {
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", () -> redis.getMappedPort(6379).toString());
+    }
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    void testWithRedisContainer() {
+        assert redis.isRunning();
+
+        redisTemplate.opsForValue().set("key", "value");
+        String value = redisTemplate.opsForValue().get("key");
+        assertEquals("value", value);
+    }
+}
